c6176fa5ccaa4808ed6b1a8a925f66f488b3e7c7
Yachen <12ys47@queensu.ca>
2017-03-30 21:21:45 -0400

    removed excess comments

--- member.php
+++ member.php
@@ -157,31 +157,5 @@ if(isset($_SESSION['id'])){
 	
 </div> <!-- close everythingContainer -->
 
-<!--
- Welcome  <?php echo $myrow['username']; ?>, <a href="index.php?logout=1">Log Out</a><br/>
-<!-- dynamic content will be here 
-<form name='editProfile' id='editProfile' action='profile.php' method='post'>
-    <table border='0'>
-        <tr>
-            <td>Username</td>
-            <td><input type='text' name='username' id='username' disabled  /></td>
-        </tr>
-        <tr>
-            <td>Password</td>
-             <td><input type='text' name='password' id='password' /></td>
-        </tr>
-		<tr>
-            <td>Email</td>
-            <td><input type='text' name='email' id='email' /></td>
-        </tr>
-        <tr>
-            <td></td>
-            <td>
-                <input type='submit' name='updateBtn' id='updateBtn' value='Update' /> 
-            </td>
-        </tr>
-    </table>
-</form>
--->
 </body>
 </html>
